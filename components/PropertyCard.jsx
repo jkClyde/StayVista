280f0205eb76607829fca3da08de5f73b2440928
jkClyde <aquin.randall@gmail.com>
2026-02-02 17:54:36 +0800

    card footer update

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -69,10 +69,10 @@ const PropertyCard = ({ property }) => {
           </div>
 
         </div>
-        <div className='border border-gray-100 mb-5'></div>
+        {/* <div className='border border-gray-100 mb-5'></div> */}
 
         {/* Card Footer */}
-        <div className='flex flex-col lg:flex-row justify-between mb-4 px-4'>
+        {/* <div className='flex flex-col lg:flex-row justify-between mb-4 px-4'>
           <div className='flex align-middle gap-2 mb-4 lg:mb-0'>
             <FaMapMarker className='text-orange-700 mt-1' />
             <span className='text-orange-700'>
@@ -86,7 +86,7 @@ const PropertyCard = ({ property }) => {
           >
             Details
           </Link>
-        </div>
+        </div> */}
       </div>
     </div>
   );
